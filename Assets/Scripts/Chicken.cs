ffe141ab6c4df6f5332161c622b2fe40c291af3e
Noah <Noah_castro25@yahoo.com>
2022-03-06 00:41:01 -0800

    Level build out, player functionality tweaked and adjusted, goal set in place

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -8,33 +8,32 @@ public class Chicken : MonoBehaviour
     [SerializeField] private Animator _anim;
     [SerializeField] private Camera _cam;
     [SerializeField] private Transform _camTransform;
-    [SerializeField] private Animator _followTarget;
+    [SerializeField] private Transform _followTarget;
 
-    // [SerializeField] LayerMask _aimLayerMask;
+    private Vector3 respawnPosition;
+    private Quaternion respawnRotation;
 
     private bool _flipped;
     private bool _canFlip = true;
     private bool _isMoving;
-    private CharacterController charac;
     private float _turnSmoothVelocity;
     private Vector3 _direction;
+
+    private void Awake() {
+        respawnPosition = transform.position;
+        respawnRotation = transform.rotation;
+    }
 
     void FixedUpdate() => Move();   
     
     private void Move() {
-
-        // AimTowardMouse();
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         _direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(_direction.magnitude >= 0.1f) {
-            // if(!_isMoving) {
-                _anim.Play("Run In Place");
-                //  _isMoving = true;
-            // }
-            // else _anim.Play("Idle");
+            _anim.Play("Run In Place");
            
             float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _camTransform.eulerAngles.y;
             float moveAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, .1f);
@@ -43,15 +42,8 @@ public class Chicken : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             GetComponent<Rigidbody>().MovePosition(transform.position + moveDirection.normalized * Time.fixedDeltaTime * 3f);
         } 
-        else {
-            // if(_hasBox) 
-                _anim.Play("Idle");
-                // _isMoving = false;
-            // else _anim.Play("Breathing Idle");
-        }
-
-        if(Input.GetKey("space"))
-            _anim.Play("Eat");
+        else
+             _anim.Play("Idle");
 
         if(Input.GetKey("r") && _canFlip)
             FlipGravity();
@@ -62,36 +54,47 @@ public class Chicken : MonoBehaviour
 
         if(_flipped) {
             Physics.gravity = new Vector3(0, -9.81f, 0);
-            _followTarget.Play("FlipBack");
-
+            StartCoroutine(FlipDelay());     
             _flipped = false;
         }
         else {
             Physics.gravity = new Vector3(0, 9.81f, 0);
-            _followTarget.Play("FlipAnim");
+             StartCoroutine(FlipBackDelay());
             _flipped = true;
         }
     }
-    
+
+     private IEnumerator FlipDelay() {
+        yield return new WaitForSeconds(0.3f);
+        _followTarget.Rotate(0, 0, 180); 
+     }
+
+      private IEnumerator FlipBackDelay() {
+        yield return new WaitForSeconds(0.3f);
+        _followTarget.Rotate(0, 0, -180); 
+     }
+        
     private IEnumerator FlipCoroutine() {
         _canFlip = false;
-        yield return new WaitForSeconds(2f);
+        _anim.enabled = false;
+        yield return new WaitForSeconds(.75f);
         _canFlip = true;
+        _anim.enabled = true;
     }
-
-    // private void AimTowardMouse() {
-    //     Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-
-    //     if(Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, _aimLayerMask)) {
-    //         var direction = hitInfo.point - transform.position;
-    //         direction.y = 0f;
-    //         direction.Normalize();
-    //         transform.forward = direction;
-    //     }
-    // }
-
+    
     private void OnTriggerEnter(Collider other) {
-        // if(other.CompareTag("item"))
-            // other.gameObject.GetComponent<Rigidbody>().AddForce(0, 10, 0);
+        if(other.CompareTag("Obstacle")) {
+            Physics.gravity = new Vector3(0, -9.81f, 0);
+            if(_flipped) {
+                _followTarget.Rotate(0, 0, -180);
+                _flipped = false;
+            }
+
+            transform.rotation = respawnRotation;
+            transform.position = respawnPosition;
+        }
+        // if(other.CompareTag("Egg"))
+            // Increment Limit
+        
     }
 }
